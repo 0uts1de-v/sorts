4f44c20e26caa70c48965c15ac594ee93c6b5a3b
0uts1de <www.yusukekaji@gmail.com>
2019-02-14 01:41:35 +0900

    add initialize

---
+++ src/shaker.cpp
@@ -0,0 +1,29 @@
+ #include <iostream>
+#include <vector>
+#include <random>
+#include <algorithm>
+
+using namespace std;
+
+int init_vec(vector<int> &vec){
+    for (int i = 0; i < vec.size(); ++i){
+        vec.at(i) = i;
+    }
+    random_device rd;
+    mt19937 shuffle_engine(rd());
+    shuffle(vec.begin(), vec.end(), shuffle_engine);
+    return 0;
+}
+
+int main(){
+    vector<int> vec(100);
+    init_vec(vec);
+    
+    
+    
+    for (int &i : vec){
+        cout << i << endl;
+    }
+    return 0;
+}
+
