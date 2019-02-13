d8f1a508277da68f09635cc87aab5f8e816b4a95
0uts1de <www.yusukekaji@gmail.com>
2019-02-14 07:58:33 +0900

    update bucket

--- src/bucket.cpp
+++ src/bucket.cpp
@@ -15,11 +15,26 @@ int init_vec(vector<int> &vec){
     return 0;
 }
 
+int bucket_sort(vector<int> &vec){
+    vector<int> bucket(vec.size());
+    for (int i = 0; i < vec.size(); ++i){
+        ++bucket.at(vec.at(i));
+    }
+    int k = 0;
+    for (int i = 0; i < vec.size(); ++i){
+        for (int j = 0; j < bucket.at(i); ++j){
+            if (j > 0) ++k;
+            vec.at(i + k) = i;
+        }
+    }
+    return 0;
+}
+
 int main(){
     vector<int> vec(100);
     init_vec(vec);
     
-    
+    bucket_sort(vec);
     
     for (int &i : vec){
         cout << i << endl;
