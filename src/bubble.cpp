d9654e0abb5a72637b6315be2837776a607668ce
0uts1de <www.yusukekaji@gmail.com>
2019-02-14 02:55:41 +0900

    update bubble

--- src/bubble.cpp
+++ src/bubble.cpp
@@ -15,11 +15,28 @@ int init_vec(vector<int> &vec){
     return 0;
 }
 
+int bubble_sort(vector<int> &vec){
+    bool swapped;
+    while (true){
+        swapped = false;
+        for (int i = 0; i < vec.size() - 1; ++i){
+            if (vec.at(i) > vec.at(i + 1)){
+                swap(vec.at(i), vec.at(i + 1));
+                swapped = true;
+            }
+        }
+        if (!swapped){
+            break;
+        }
+    }
+    return 0;
+}
+
 int main(){
     vector<int> vec(100);
     init_vec(vec);
     
-    
+    bubble_sort(vec);
     
     for (int &i : vec){
         cout << i << endl;
