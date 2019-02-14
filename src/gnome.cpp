c2e50effd94ac3f69cf7a6854fbfee3cffe3df4e
0uts1de <www.yusukekaji@gmail.com>
2019-02-14 16:30:09 +0900

    update gnome

--- src/gnome.cpp
+++ src/gnome.cpp
@@ -15,11 +15,26 @@ int init_vec(vector<int> &vec){
     return 0;
 }
 
+int gnome_sort(vector<int> &vec){
+    int i = 0;
+    while (i + 1 < vec.size()){
+        if (vec.at(i) < vec.at(i + 1)){
+            ++i;
+        }
+        else{
+            swap(vec.at(i), vec.at(i + 1));
+            --i;
+            if (i == -1) ++i;
+        }
+    }
+    return 0;
+}
+
 int main(){
     vector<int> vec(100);
     init_vec(vec);
     
-    
+    gnome_sort(vec);
     
     for (int &i : vec){
         cout << i << endl;
