d30c59c94522ec74817c4743f1d639950aa305cb
yu_u_su_ke <yu_u_su_ke@outlook.jp>
2019-02-17 20:04:39 +0900

    add comb11

---
+++ src/comb11.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <algorithm>
+
+using namespace std;
+
+int init_vec(vector<int> &vec)
+{
+    for (int i = 0; i < vec.size(); ++i)
+    {
+        vec.at(i) = i;
+    }
+    random_device rd;
+    mt19937 shuffle_engine(rd());
+    shuffle(vec.begin(), vec.end(), shuffle_engine);
+    return 0;
+}
+
+int comb_sort(vector<int> &vec)
+{
+    int h = vec.size() / 1.3;
+    bool swapped;
+    while (true)
+    {
+        swapped = false;
+        for (int i = 0; i + h < vec.size(); ++i)
+        {
+            if (vec.at(i) > vec.at(i + h))
+            {
+                swap(vec.at(i), vec.at(i + h));
+                swapped = true;
+            }
+        }
+        if (!swapped && h == 1)
+            break;
+        if (h != 1)
+            h /= 1.3;
+        if (h == 9 || h = 10)
+            h = 11;
+    }
+    return 0;
+}
+
+int main()
+{
+    vector<int> vec(100);
+    init_vec(vec);
+
+    comb_sort(vec);
+
+    for (int &i : vec)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
